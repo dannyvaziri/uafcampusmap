1892c3dfd019381ea5b81ea065a0f7d6b1e3782e
dannyvaziri <dannyvaziri@gmail.com>
2026-09-01 16:59:36 -0800

    Load UAF brand stylesheet after core styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import 'leaflet/dist/leaflet.css'
 import './styles.css'
+import './brand.css'
 import App from './App.jsx'
 import {applyMapConfig,loadPublishedConfig} from './data/runtime.js'
 
